6b740cd72baf12e006f43a031b56aca83d223bfa
Abd Errahmane Meqdad <abderahmanmekdad1@gmail.com>
2026-02-19 11:23:08 +0000

    add the model and the migration 'Transaction'

---
+++ database/migrations/2026_02_19_112155_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('property_id')->constrained()->onDelete('cascade');
+            $table->foreignId('client_id')->constrained('users')->onDelete('cascade');
+            $table->enum('transaction_type', ['vente','location','location_vacance']);
+            $table->decimal('price', 10, 2);
+            $table->enum('status', ['pending','completed','cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
